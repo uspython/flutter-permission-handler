359526eecb05da17a4f027816ea5278bf1b6428d
Jeff Zhao <jeff40577@gmail.com>
2019-05-13 14:55:28 +0800

    * remove source code those permission are not need on iOS FOR NOW

--- ios/Classes/PermissionManager.h
+++ ios/Classes/PermissionManager.h
@@ -9,15 +9,20 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-#import "AudioVideoPermissionStrategy.h"
-#import "ContactPermissionStrategy.h"
-#import "EventPermissionStrategy.h"
-#import "LocationPermissionStrategy.h"
-#import "MediaLibraryPermissionStrategy.h"
-#import "PermissionStrategy.h"
-#import "PhotoPermissionStrategy.h"
-#import "SensorPermissionStrategy.h"
-#import "SpeechPermissionStrategy.h"
+/**
+ * uncomment these strategies if you really need.
+ 
+ #import "AudioVideoPermissionStrategy.h"
+ #import "ContactPermissionStrategy.h"
+ #import "EventPermissionStrategy.h"
+ #import "LocationPermissionStrategy.h"
+ #import "MediaLibraryPermissionStrategy.h"
+ #import "PermissionStrategy.h"
+ #import "PhotoPermissionStrategy.h"
+ #import "SensorPermissionStrategy.h"
+ #import "SpeechPermissionStrategy.h"
+ */
+
 #import "UnknownPermissionStrategy.h"
 #import "Enums.h"
 #import "Codec.h"
